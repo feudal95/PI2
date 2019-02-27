792080cbaf10b1a1f94d1404ab6a58eab7d0b2cb
Diego <diegoeduardo-88@hotmail.com>
2019-02-27 02:12:05 -0600

    se esta trabajando en dar de alta salones, el query ya esta

--- PI2/agregarSalon.Designer.cs
+++ PI2/agregarSalon.Designer.cs
@@ -33,12 +33,20 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.salon = new System.Windows.Forms.TextBox();
+            this.materia = new System.Windows.Forms.TextBox();
+            this.semestre = new System.Windows.Forms.TextBox();
+            this.hE = new System.Windows.Forms.TextBox();
+            this.hF = new System.Windows.Forms.TextBox();
             this.usuario = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lunes = new System.Windows.Forms.CheckBox();
+            this.martes = new System.Windows.Forms.CheckBox();
+            this.miercoles = new System.Windows.Forms.CheckBox();
+            this.jueves = new System.Windows.Forms.CheckBox();
+            this.viernes = new System.Windows.Forms.CheckBox();
+            this.sabado = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -46,18 +54,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(46, 94);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "salon";
+            this.label1.Text = "Salon";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(46, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "materia";
+            this.label2.Text = "Materia";
             // 
             // label3
             // 
@@ -86,40 +94,40 @@
             this.label5.TabIndex = 4;
             this.label5.Text = "Semestre";
             // 
-            // textBox1
+            // salon
             // 
-            this.textBox1.Location = new System.Drawing.Point(103, 94);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
+            this.salon.Location = new System.Drawing.Point(103, 94);
+            this.salon.Name = "salon";
+            this.salon.Size = new System.Drawing.Size(100, 20);
+            this.salon.TabIndex = 5;
             // 
-            // textBox2
+            // materia
             // 
-            this.textBox2.Location = new System.Drawing.Point(103, 140);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 6;
+            this.materia.Location = new System.Drawing.Point(103, 140);
+            this.materia.Name = "materia";
+            this.materia.Size = new System.Drawing.Size(100, 20);
+            this.materia.TabIndex = 6;
             // 
-            // textBox3
+            // semestre
             // 
-            this.textBox3.Location = new System.Drawing.Point(103, 182);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.semestre.Location = new System.Drawing.Point(103, 182);
+            this.semestre.Name = "semestre";
+            this.semestre.Size = new System.Drawing.Size(100, 20);
+            this.semestre.TabIndex = 7;
             // 
-            // textBox4
+            // hE
             // 
-            this.textBox4.Location = new System.Drawing.Point(147, 221);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(56, 20);
-            this.textBox4.TabIndex = 8;
+            this.hE.Location = new System.Drawing.Point(147, 221);
+            this.hE.Name = "hE";
+            this.hE.Size = new System.Drawing.Size(56, 20);
+            this.hE.TabIndex = 8;
             // 
-            // textBox5
+            // hF
             // 
-            this.textBox5.Location = new System.Drawing.Point(346, 221);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(56, 20);
-            this.textBox5.TabIndex = 9;
+            this.hF.Location = new System.Drawing.Point(346, 221);
+            this.hF.Name = "hF";
+            this.hF.Size = new System.Drawing.Size(56, 20);
+            this.hF.TabIndex = 9;
             // 
             // usuario
             // 
@@ -130,17 +138,111 @@
             this.usuario.TabIndex = 10;
             this.usuario.Text = "label6";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lunes
+            // 
+            this.lunes.AutoSize = true;
+            this.lunes.Location = new System.Drawing.Point(123, 284);
+            this.lunes.Name = "lunes";
+            this.lunes.Size = new System.Drawing.Size(32, 17);
+            this.lunes.TabIndex = 12;
+            this.lunes.Text = "L";
+            this.lunes.UseVisualStyleBackColor = true;
+            this.lunes.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // martes
+            // 
+            this.martes.AutoSize = true;
+            this.martes.Location = new System.Drawing.Point(161, 284);
+            this.martes.Name = "martes";
+            this.martes.Size = new System.Drawing.Size(35, 17);
+            this.martes.TabIndex = 13;
+            this.martes.Text = "M";
+            this.martes.UseVisualStyleBackColor = true;
+            this.martes.CheckedChanged += new System.EventHandler(this.martes_CheckedChanged);
+            // 
+            // miercoles
+            // 
+            this.miercoles.AutoSize = true;
+            this.miercoles.Location = new System.Drawing.Point(202, 284);
+            this.miercoles.Name = "miercoles";
+            this.miercoles.Size = new System.Drawing.Size(35, 17);
+            this.miercoles.TabIndex = 14;
+            this.miercoles.Text = "M";
+            this.miercoles.UseVisualStyleBackColor = true;
+            this.miercoles.CheckedChanged += new System.EventHandler(this.miercoles_CheckedChanged);
+            // 
+            // jueves
+            // 
+            this.jueves.AutoSize = true;
+            this.jueves.Location = new System.Drawing.Point(243, 284);
+            this.jueves.Name = "jueves";
+            this.jueves.Size = new System.Drawing.Size(31, 17);
+            this.jueves.TabIndex = 15;
+            this.jueves.Text = "J";
+            this.jueves.UseVisualStyleBackColor = true;
+            this.jueves.CheckedChanged += new System.EventHandler(this.jueves_CheckedChanged);
+            // 
+            // viernes
+            // 
+            this.viernes.AutoSize = true;
+            this.viernes.Location = new System.Drawing.Point(280, 284);
+            this.viernes.Name = "viernes";
+            this.viernes.Size = new System.Drawing.Size(33, 17);
+            this.viernes.TabIndex = 16;
+            this.viernes.Text = "V";
+            this.viernes.UseVisualStyleBackColor = true;
+            this.viernes.CheckedChanged += new System.EventHandler(this.viernes_CheckedChanged);
+            // 
+            // sabado
+            // 
+            this.sabado.AutoSize = true;
+            this.sabado.Location = new System.Drawing.Point(319, 284);
+            this.sabado.Name = "sabado";
+            this.sabado.Size = new System.Drawing.Size(33, 17);
+            this.sabado.TabIndex = 17;
+            this.sabado.Text = "S";
+            this.sabado.UseVisualStyleBackColor = true;
+            this.sabado.CheckedChanged += new System.EventHandler(this.sabado_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Frecuencia";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
             // agregarSalon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.sabado);
+            this.Controls.Add(this.viernes);
+            this.Controls.Add(this.jueves);
+            this.Controls.Add(this.miercoles);
+            this.Controls.Add(this.martes);
+            this.Controls.Add(this.lunes);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.usuario);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hF);
+            this.Controls.Add(this.hE);
+            this.Controls.Add(this.semestre);
+            this.Controls.Add(this.materia);
+            this.Controls.Add(this.salon);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -161,11 +263,19 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox salon;
+        private System.Windows.Forms.TextBox materia;
+        private System.Windows.Forms.TextBox semestre;
+        private System.Windows.Forms.TextBox hE;
+        private System.Windows.Forms.TextBox hF;
         private System.Windows.Forms.Label usuario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox lunes;
+        private System.Windows.Forms.CheckBox martes;
+        private System.Windows.Forms.CheckBox miercoles;
+        private System.Windows.Forms.CheckBox jueves;
+        private System.Windows.Forms.CheckBox viernes;
+        private System.Windows.Forms.CheckBox sabado;
+        private System.Windows.Forms.Label label6;
     }
 }
--- PI2/agregarSalon.cs
+++ PI2/agregarSalon.cs
@@ -16,15 +16,23 @@ namespace PI2
     {
         Thread th;
         string mat = "";
+        string docenteID = "";
+        string lun = "0";
+        string mar = "0";
+        string mie = "0";
+        string jue = "0";
+        string vie = "0";
+        string sab = "0";
         MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=asistencia;SslMode=none");
         public agregarSalon(string welcm)
         {
             mat = welcm;
             InitializeComponent();
-            MySqlDataAdapter sda = new MySqlDataAdapter("SELECT nombreDocente from docentes WHERE matricula = '" + welcm + "';", conn);
+            MySqlDataAdapter sda = new MySqlDataAdapter("SELECT nombreDocente, id from docentes WHERE matricula = '" + welcm + "';", conn);
             DataTable dt = new DataTable();
             sda.Fill(dt);
             string welcom = dt.Rows[0][0].ToString();
+            docenteID = dt.Rows[0][1].ToString();
             usuario.Text = "Docente: " + welcom;
  
         }
@@ -32,6 +40,88 @@ namespace PI2
         private void agregarSalon_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO `asistencia`.`salon` (`num_salon`, `materia`, `horaInicio`, `horaFinal`, `semestre`, `lunes`, `martes`, `miercoles`, `jueves`, `viernes`, `sabado`, `docentes_id`) VALUES ('" + salon.Text+ "', '"+materia.Text+"', '"+semestre.Text+"', '"+hE.Text+"', '"+hF.Text+ "','"+lun+"', '"+mar+"', '"+mie+"', '"+jue+"', '"+vie+"','"+sab+"', '" + docenteID+"');";
+            MessageBox.Show(query);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (lunes.Checked == true)
+            {
+                lun = "1";
+            }else if (lunes.Checked == false)
+            {
+                lun = "0";
+            }
+        }
+
+        private void martes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (martes.Checked == true)
+            {
+                mar = "1";
+            }
+            else if (martes.Checked == false)
+            {
+                mar = "0";
+            }
+        }
+
+        private void miercoles_CheckedChanged(object sender, EventArgs e)
+        {
+            if (miercoles.Checked == true)
+            {
+                mie = "1";
+            }
+            else if (miercoles.Checked == false)
+            {
+                mie = "0";
+            }
+        }
+
+        private void jueves_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jueves.Checked == true)
+            {
+                jue = "1";
+            }
+            else if (jueves.Checked == false)
+            {
+                jue = "0";
+            }
+        }
+
+        private void viernes_CheckedChanged(object sender, EventArgs e)
+        {
+            if (viernes.Checked == true)
+            {
+                vie = "1";
+            }
+            else if (viernes.Checked == false)
+            {
+                vie = "0";
+            }
+        }
+
+        private void sabado_CheckedChanged(object sender, EventArgs e)
+        {
+            if (sabado.Checked == true)
+            {
+                sab = "1";
+            }
+            else if (sabado.Checked == false)
+            {
+                sab ="0";
+            }
         }
     }
 }
